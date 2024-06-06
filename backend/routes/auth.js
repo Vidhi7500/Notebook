d8b87718566e52c5d7dd4f3b90b60d5caf3747d2
Vidhi7500 <vidhisingh7500@gmail.com>
2024-06-06 21:40:50 +0530

    express validations done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
+const {body, validationResult} = require('express-validator');
 
-router.get('/', (req, res)=>{
-    obj = {
-        a: 'this',
-        number: 34
+
+//create a user using : post  "./api/auth/"
+router.post('/', [
+    body('name').isLength({min:3}),
+    body('email').isEmail(),
+    body('password').isLength({min:5}),
+], (req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
     }
-    res.json(obj)
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        
+    }).then(user => res.json(user))
+    .catch(err=>console.log(err))
+    res.json({error: "Please enter a unique value for email", message: err.message})
 })
 
 module.exports = router
